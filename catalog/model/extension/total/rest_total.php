ce259e07c13b67ddd92545fc66a71fb2399fdf70
kazu7391 <vinhtq.0703@gmail.com>
2022-06-30 00:08:38 +0700

    Check upfront total

--- catalog/model/extension/total/rest_total.php
+++ catalog/model/extension/total/rest_total.php
@@ -2,12 +2,14 @@
 class ModelExtensionTotalRestTotal extends Model {
 	public function getTotal($total) {
 		$this->load->language('extension/total/rest_total');
-
-		$total['totals'][] = array(
-			'code'       => 'rest_total',
-			'title'      => $this->language->get('text_rest_total'),
-			'value'      => max(0, $total['total']),
-			'sort_order' => $this->config->get('total_rest_total_sort_order')
-		);
+		
+		if($this->config->get('config_partial_payments_status')) {
+			$total['totals'][] = array(
+				'code'       => 'rest_total',
+				'title'      => $this->language->get('text_rest_total'),
+				'value'      => max(0, $total['total']),
+				'sort_order' => $this->config->get('total_rest_total_sort_order')
+			);
+		}
 	}
 }
--- catalog/model/extension/total/upfront_payment.php
+++ catalog/model/extension/total/upfront_payment.php
@@ -4,15 +4,23 @@ class ModelExtensionTotalUpfrontPayment extends Model
     public function getTotal($total) {
 		$this->load->language('extension/total/upfront_payment');
 
-		$upfront_payment_value = 10;
+		if($this->config->get('config_partial_payments_status')) {
+			$upfront_payment_status = (int) $this->config->get('config_partial_payments_status');
+			
+			if($upfront_payment_status) {
+				$partial_payments_minimum = (float) $this->config->get('config_partial_payments_minimum');
 
-		$total['totals'][] = array(
-			'code'       => 'upfront_payment',
-			'title'      => $this->language->get('text_upfront_payment'),
-			'value'      => $upfront_payment_value,
-			'sort_order' => $this->config->get('total_upfront_payment_sort_order')
-		);
+				$upfront_payment_value = $partial_payments_minimum;
 
-		$total['total'] -= $upfront_payment_value;
+				$total['totals'][] = array(
+					'code'       => 'upfront_payment',
+					'title'      => $this->language->get('text_upfront_payment'),
+					'value'      => $upfront_payment_value,
+					'sort_order' => $this->config->get('total_upfront_payment_sort_order')
+				);
+
+				$total['total'] -= $upfront_payment_value;
+			}
+		}
 	}
 }
